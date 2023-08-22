969abfe0e7dc36952b19042d2994feaa39da28f2
Yusuke Uchida <fantomyuu0623@gmail.com>
2023-08-22 16:58:05 +0900

    llvm/programs/visualize-cfg.sh:
    - Changed llfile
    - Added clang option (-O2)
    - Changed to create pdf instead of png

--- llvm/programs/visualize-cfg.sh
+++ llvm/programs/visualize-cfg.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 input=$1
-llfile="$(basename "${input}" ".c").ll"
+llfile="${input}.ll"
 echo "$input"
 
 # .c -> .dot
-clang -S -emit-llvm "$1" -o "$llfile"
+clang -S -emit-llvm "$1" -O2 -o "$llfile"
 opt -dot-cfg -disable-output -enable-new-pm=0 "$llfile" --cfg-dot-filename-prefix="$input"
 
 # *.dot -> .png
-find . -type f -name "${input}*.dot" -print0 | xargs -I {} -0 dot -Tpng {} -o "$(basename {} ".dot")".png
+find . -type f -name "${input}*.dot" -print0 | xargs -I {} -0 dot -Tpdf {} -o "$(basename {} ".dot")".pdf
